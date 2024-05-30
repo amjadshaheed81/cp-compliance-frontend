932e4a8b9f91739dc39ebcab1d92697ed70204b8
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-05-30 10:45:27 +0530

    feat: add contracts

--- src/components/Protected/Sites/Projects/Contractors.jsx
+++ src/components/Protected/Sites/Projects/Contractors.jsx
@@ -13,6 +13,7 @@ const Contractors = ({}) => {
   };
   const style = {
     position: "absolute",
+    overflow: 'auto',
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
@@ -91,21 +92,126 @@ const Contractors = ({}) => {
             View Contract &amp; Quote
           </Typography>
           <form class="row border-top">
-            <div class="col-md-12">
-              
+            <div>
+              <span class="badge bg-warning">Recieved</span>
+            </div>
+            <div className="col-md-6">
+              <label for="projectSummary" class="form-label">
+                Project Summary
+              </label>
+              <input
+                type="text"
+                name="projectSummary"
+                class="form-control"
+                id="projectSummary"
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="contractor" class="form-label">
+                Contractor
+              </label>
+              <input
+                type="text"
+                name="contractor"
+                class="form-control"
+                id="contractor"
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="projectManager" class="form-label">
+                Project Manager
+              </label>
+              <input
+                type="text"
+                name="projectManager"
+                class="form-control"
+                id="projectManager"
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="projectStartDate" class="form-label">
+                Project Start date
+              </label>
+              <input
+                type="date"
+                name="projectStartDate"
+                class="form-control"
+                id="projectStartDate"
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="quoteDate" class="form-label">
+                Quote date
+              </label>
+              <input
+                type="date"
+                name="quoteDate"
+                class="form-control"
+                id="quoteDate"
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="quote" class="form-label">
+                Quote (GBP)
+              </label>
+              <input type="text" name="quote" class="form-control" id="quote" />
+            </div>
+            <div className="col-md-6">
+              <label for="officialQuote" class="form-label">
+                Official Quote
+              </label>
+              <input
+                type="text"
+                name="officialQuote"
+                class="form-control"
+                id="officialQuote"
+              />
+            </div>
+            <div className="col-md-6">
+              <label for="notes" class="form-label">
+                Notes
+              </label>
+              <textarea name="notes" class="form-control" id="notes"></textarea>
+            </div>
+            <div>
+              <table class="table f-11">
+                <thead class="table-dark">
+                  <tr>
+                    <th scope="col">Mandatory Folder</th>
+                    <th scope="col">File</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Asbestos Removal Work</td>
+                    <td>
+                      <span class="badge bg-light text-primary">File1.pdf</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
             <div className="col-md-12 pt-4 border-top">
               <div class="float-end">
-                        <button type="button" class="btn btn-light mb-3 mr-4 text-primary">
-                          Cancel
-                        </button>
-                        &nbsp; &nbsp;
-                        <button type="submit" class="btn btn-primary mb-3 mr-4">
-                          Save
-                        </button>
-                      </div>
-
+                <button
+                  type="button"
+                  class="btn btn-light mb-3 mr-4 text-primary"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Close
+                </button>
+                &nbsp; &nbsp;
+                <button type="button" class="btn btn-success mb-3 mr-4">
+                  Approve
+                </button>
+                &nbsp; &nbsp;
+                <button type="button" class="btn btn-danger mb-3 mr-4">
+                  Reject
+                </button>
               </div>
+            </div>
           </form>
         </Box>
       </Modal>
