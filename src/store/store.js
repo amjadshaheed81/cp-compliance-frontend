ef931db4fd77fc277cf91a29b400cb5e9fdb6e32
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-07-16 00:48:01 +0530

    fix: linked redux state with local storage to resolve the refresh issue across application

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,21 @@
 import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
+import { saveState, loadState } from "./localStorage";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// Load persisted state from localStorage
+const persistedState = loadState();
+
+// Create store with persisted state
+const store = createStore(
+  rootReducer,
+  persistedState,
+  applyMiddleware(thunk)
+);
+
+// Save state to localStorage whenever the store changes
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
