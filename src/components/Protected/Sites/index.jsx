c008f9d3a4624318d3029b9465c3f9ab01142880
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-07-29 18:57:13 +0530

    fix: CPCOM-184

--- src/components/Protected/Sites/index.jsx
+++ src/components/Protected/Sites/index.jsx
@@ -278,6 +278,24 @@ const Sites = ({
                         </button>{" "}
                       </Tooltip>
                       &nbsp;
+                      {loggedInUserData?.userType === "External" &&
+                        loggedInUserData?.role !== ROLE.ADMIN && (
+                          <Tooltip title={`Edit ${itm?.siteName}`} arrow>
+                            <button
+                              className="btn btn-sm btn-light"
+                              onClick={() => {
+                                setTimeout(() => {
+                                  goTo(
+                                    `/update-site?siteId=${itm?.siteId}&isViewMode=edit`
+                                  );
+                                }, 1000);
+                                updateSiteData({ ...itm, isViewMode: false });
+                              }}
+                            >
+                              <i className="fas fa-pen"></i>
+                            </button>{" "}
+                          </Tooltip>
+                        )}
                       {loggedInUserData?.role === ROLE.ADMIN && (
                         <Fragment>
                           <Tooltip title={`Edit ${itm?.siteName}`} arrow>
