58fe3dba88c156b611d16b180409167c2cedc395
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-08-02 00:42:08 +0530

    fix: CPCOM-225

--- src/components/Protected/Users/TagSites.jsx
+++ src/components/Protected/Users/TagSites.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import { connect } from "react-redux";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import { Chip } from "@mui/material";
+import Pagination from "../../common/Pagination/Pagination";
 
 export const ITEM_HEIGHT = 48;
 export const ITEM_PADDING_TOP = 8;
@@ -18,26 +18,51 @@ export const MenuProps = {
 };
 
 const TagSites = ({ showSiteTagModal, setShowSiteTagModal, taggedSites }) => {
-    console.log("taggedSites", taggedSites)
   const handleOpen = () => setShowSiteTagModal(true);
   const handleClose = () => setShowSiteTagModal(false);
+  const [sitesPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastSite = currentPage * sitesPerPage;
+  const indexOfFirstSite = indexOfLastSite - sitesPerPage;
+  const currentSites = taggedSites.slice(indexOfFirstSite, indexOfLastSite);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
     <React.Fragment>
       <Dialog
         open={showSiteTagModal}
         onClose={handleClose}
-        maxWidth="sm"
+        maxWidth="lg"
         fullWidth
       >
         <DialogTitle> {taggedSites?.length} Site Tagged</DialogTitle>
         <DialogContent dividers>
-          {taggedSites?.map(itm => (
-            <Chip
-              color={'primary'}
-              label={itm?.name ? itm?.name : itm}
-              style={{ marginLeft: "10px", marginTop: "10px" }}
+          <div className="row p-2">
+            <div className="col-md-12 table-responsive">
+              <table className="table" style={{ border: "1px solid" }}>
+                <thead className="table-dark">
+                  <tr>
+                    <th scope="col">Site Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentSites?.map((itm) => (
+                    <tr>
+                      <td>{itm?.name ? itm?.name : itm}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="row">
+            <Pagination
+              totalPages={Math.ceil(taggedSites.length / sitesPerPage)}
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
             />
-          ))}
+          </div>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} className="bg-light text-primary">
