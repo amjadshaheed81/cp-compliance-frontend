bcedb8ae44519bd2269225a405e46be23d49e19e
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-08-15 20:41:12 +0530

    fix: CPCOM-259

--- src/components/Protected/Sites/Assets/index.jsx
+++ src/components/Protected/Sites/Assets/index.jsx
@@ -57,7 +57,11 @@ const Assets = ({
 
           <Box sx={{ width: "100%", typography: "body1" }}>
             <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Box sx={{
+    '& .MuiTabs-flexContainer': {
+      flexWrap: 'wrap',
+    },
+  }}>
                 <TabList onChange={tabChange} aria-label="lab API tabs example">
                   <Tab label="Summary" value="1" />
                   <Tab label="Doors" value="2" />
