656b6a1466d2f79e0fa9c43f3c095e31d2fa6f31
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-08-19 02:16:35 +0530

    fix: add uploader id to bulk upload

--- src/components/Protected/Sites/Documents/BulkUpload.jsx
+++ src/components/Protected/Sites/Documents/BulkUpload.jsx
@@ -53,6 +53,7 @@ const BulkUpload = ({
               originalFileName: iterator?.name,
               fileVersion: version,
               siteId: siteSelectedForGlobal?.siteId,
+              uploaderUserId: loggedInUserData?.id,
               reviewerUserId: loggedInUserData?.id,
               uploadDate: `${moment(new Date()).format("YYYY-MM-DD")} 10:00:00`,
               issueDate: `${moment(new Date()).format("YYYY-MM-DD")} 10:00:00`,
