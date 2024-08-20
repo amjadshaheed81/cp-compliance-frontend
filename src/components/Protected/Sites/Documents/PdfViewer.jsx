f34961c6d55fa8b4a61a93fc3d58bfce6abd8d3c
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-08-21 02:26:43 +0530

    fix: CPCOM-167

--- src/components/Protected/Sites/Documents/PdfViewer.jsx
+++ src/components/Protected/Sites/Documents/PdfViewer.jsx
@@ -18,9 +18,45 @@ import FileViewer from "./FileViewer";
 
 const PdfViewer = ({ showPdfModal, setShowPdfModal, selectedPdf }) => {
   const [open, setOpen] = useState(showPdfModal);
+  const [extension, setExtension] = useState("");
   const handleOpen = () => setShowPdfModal(true);
   const handleClose = () => setShowPdfModal(false);
+  const downloadFile = (event, fileName) => {
+    event.preventDefault();
+    if (extension === ".pdf") {
+      fetch(selectedPdf).then((response) => {
+        response.blob().then((blob) => {
+          // Creating new object of PDF file
+          const fileURL = window.URL.createObjectURL(blob);
 
+          // Setting various property values
+          let alink = document.createElement("a");
+          alink.href = fileURL;
+          alink.download = `${fileName}${extension}`;
+          alink.click();
+        });
+      });
+    } else {
+      const link = document.createElement("a");
+      link.href = selectedPdf;
+      link.download = `${fileName}${extension}`;
+      link.click();
+    }
+  };
+  useEffect(() => {
+    if (selectedPdf.includes("pdf")) {
+      setExtension(".pdf");
+    }
+    if (selectedPdf.includes("jpg")) {
+      setExtension(".jpg");
+    }
+    if (selectedPdf.includes("jpeg")) {
+      setExtension(".jpeg");
+    }
+    if (selectedPdf.includes("png")) {
+      setExtension(".png");
+    }
+  }, []);
   return (
     <>
       <Dialog open={open} maxWidth="lg" fullWidth onClose={handleClose}>
@@ -28,7 +64,7 @@ const PdfViewer = ({ showPdfModal, setShowPdfModal, selectedPdf }) => {
         <DialogContent>
           <div className="row">
             <div className="col-md-12 text-center">
-              <FileViewer fileUrl={selectedPdf}/>
+              <FileViewer fileUrl={selectedPdf} />
             </div>
           </div>
         </DialogContent>
@@ -38,8 +74,7 @@ const PdfViewer = ({ showPdfModal, setShowPdfModal, selectedPdf }) => {
           </Button>
           <a
             className="btn btn-primary bg-primary text-light"
-            href={selectedPdf}
-            download
+            onClick={(event) => downloadFile(event, "file-viewer")}
           >
             Download
           </a>
