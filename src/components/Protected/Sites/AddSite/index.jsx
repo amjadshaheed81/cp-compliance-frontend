99fcf046b520d0647f19b8d89822d826d8293cf8
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-09-20 01:02:31 +0530

    fix: cancel button issue fix

--- src/components/Protected/Sites/AddSite/index.jsx
+++ src/components/Protected/Sites/AddSite/index.jsx
@@ -336,6 +336,9 @@ const AddSite = ({
                         <button
                           type="button"
                           className="btn btn-light mb-3 mr-4"
+                          onClick={() => {
+                            navigate("/sites")
+                          }}
                         >
                           Cancel
                         </button>
