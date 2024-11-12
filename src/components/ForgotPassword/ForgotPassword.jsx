28a9c97dbe43dfe41157ffb1625d73a7736587d9
Dheeraj Kumar <dheerajrimtian@gmail.com>
2024-11-12 23:22:34 +0530

    fix: reset password safari compatible issue

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -10,10 +10,10 @@ import { post } from "../../api";
 import { toast } from "react-toastify";
 
 const ForgotPassword = ({ login }) => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [otpSend, setotpSend] = useState(false);
-  const [otp, setotp] = useState();
+  const [otp, setotp] = useState("");
   const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
@@ -28,30 +28,30 @@ const ForgotPassword = ({ login }) => {
     setotp(e.target.value);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     setotpSend(false);
-  },[])
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-  
-    const body = {email,otp,password};
-    post("/api/user/reset-password", body).then(res => {
-      setotpSend(true);
-      if(otp) {
-        setotp();
-        setPassword();
-        setEmail();
-        setotpSend(false);
-        navigate("/login");
-        toast.success("Password changed successfully!");
-      }
-    }).catch(err =>{
-      setotp();
-      toast.error(err?.response?.data?.message);
-      return;
-    })
-   
-     
+
+    const body = { email, otp, password };
+    post("/api/user/reset-password", body)
+      .then((res) => {
+        setotpSend(true);
+        if (otp) {
+          setotp("");
+          setPassword("");
+          setEmail("");
+          setotpSend(false);
+          navigate("/login");
+          toast.success("Password changed successfully!");
+        }
+      })
+      .catch((err) => {
+        setotp("");
+        toast.error(err?.response?.data?.message);
+      });
   };
 
   return (
@@ -62,54 +62,65 @@ const ForgotPassword = ({ login }) => {
             <img src={logoImage} alt="login left panel logo" />
           </div>
           <div className="col-md-6 right-panel bg-white">
-            <button className="btn btn-small btn-default back-btn"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/login");
-            }}
-            >Back</button>
+            <button
+              className="btn btn-small btn-default back-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/login");
+              }}
+            >
+              Back
+            </button>
             <form onSubmit={handleSubmit}>
               <div className="text-center mb-4">
                 <img src={logo} alt="main cpc portal logo" />
               </div>
               <h2 className="mb-2">Reset Password</h2>
               <small>
-                {otpSend ? "Verify security code" : "Enter your email address to recieve a password reset link."}
+                {otpSend
+                  ? "Verify security code"
+                  : "Enter your email address to receive a password reset link."}
               </small>
               <div className="form-group mt-2">
-              {!otpSend &&<label for="email">Email</label>}
-                {!otpSend && <input
-                  type="text"
-                  className="form-control"
-                  id="email"
-                  value={email}
-                  placeholder="Enter your email"
-                  onChange={handleEmailChange}
-                />}
+                {!otpSend && <label htmlFor="email">Email</label>}
+                {!otpSend && (
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    placeholder="Enter your email"
+                    onChange={handleEmailChange}
+                  />
+                )}
 
-{otpSend && <input
-                  type="password"
-                  className="form-control"
-                  id="email"
-                  value={password}
-                  placeholder="Enter new password"
-                  onChange={handlePasswordChange}
-                />}
-                {otpSend && <input
-                  type="text"
-                  className="form-control"
-                  id="otp"
-                  value={otp}
-                  placeholder="Enter otp"
-                  onChange={handleOtoChange}
-                />}
+                {otpSend && (
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+                    placeholder="Enter new password"
+                    onChange={handlePasswordChange}
+                  />
+                )}
+                {otpSend && (
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="otp"
+                    value={otp}
+                    placeholder="Enter otp"
+                    onChange={handleOtoChange}
+                  />
+                )}
               </div>
               <div className="form-group mt-2">
                 <button
                   type="submit"
                   className="btn btn-primary rounded w-100 login-submit"
                 >
-                  {otpSend ? "Submit" : "Send Password Rest Link"}
+                  {otpSend ? "Submit" : "Send Password Reset Link"}
                 </button>
               </div>
             </form>
