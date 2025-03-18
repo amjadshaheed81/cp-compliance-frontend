06f58e18650299fc8309f50fba2f806a49000de4
Dheeraj Kumar <dheerajrimtian@gmail.com>
2025-03-19 01:15:10 +0530

    fix: set chrome 90 as minimum supported version

--- src/utils/browserCheck.js
+++ src/utils/browserCheck.js
@@ -3,7 +3,7 @@ import bowser from 'bowser';
 export const isBrowserSupported = () => {
   const browser = bowser.getParser(window.navigator.userAgent);
   return browser.satisfies({
-    chrome: '>60',
+    chrome: '>90',
     firefox: '>60',
     edge: '>15',
     safari: '>10',
