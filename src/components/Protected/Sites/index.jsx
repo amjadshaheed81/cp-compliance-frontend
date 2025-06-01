0cc45b8dc6e652073a814710187d2d6c6cec7afe
pratulcodes <pratul.makar@tdkglobal.com>
2025-06-01 21:56:14 +0530

    fix

--- src/components/Protected/Sites/index.jsx
+++ src/components/Protected/Sites/index.jsx
@@ -355,24 +355,24 @@ const Sites = ({
                         </button>{" "}
                       </Tooltip>
                       &nbsp;
-                      {(loggedInUserData?.role === ROLE.ADMIN ||
-                        loggedInUserData?.role === ROLE.PROPERTY_MANAGER) && (
-                        <Tooltip title={`Edit ${itm?.siteName}`} arrow>
-                          <button
-                            className="btn btn-sm btn-light"
-                            onClick={() => {
-                              setTimeout(() => {
-                                goTo(
-                                  `/update-site?siteId=${itm?.siteId}&isViewMode=edit`
-                                );
-                              }, 1000);
-                              updateSiteData({ ...itm, isViewMode: false });
-                            }}
-                          >
-                            <i className="fas fa-pen"></i>
-                          </button>{" "}
-                        </Tooltip>
-                      )}
+                      {loggedInUserData?.role === ROLE.ADMIN ||
+                        loggedInUserData?.role === ROLE.PROPERTY_MANAGER || (
+                          <Tooltip title={`Edit ${itm?.siteName}`} arrow>
+                            <button
+                              className="btn btn-sm btn-light"
+                              onClick={() => {
+                                setTimeout(() => {
+                                  goTo(
+                                    `/update-site?siteId=${itm?.siteId}&isViewMode=edit`
+                                  );
+                                }, 1000);
+                                updateSiteData({ ...itm, isViewMode: false });
+                              }}
+                            >
+                              <i className="fas fa-pen"></i>
+                            </button>{" "}
+                          </Tooltip>
+                        )}
                       {/* {loggedInUserData?.userType === "External" &&
                         loggedInUserData?.role !== ROLE.ADMIN && (
                           <Tooltip title={`Edit ${itm?.siteName}`} arrow>
