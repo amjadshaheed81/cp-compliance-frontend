cebbb0704cb47a6a441df1fcd3d0e49dea98389b
pratulcodes <pratul.makar@tdkglobal.com>
2025-09-26 23:41:02 +0530

    Intruder DONE✅

--- src/components/Protected/Sites/SiteChecks/AirConditioningRecurrenceCheck.jsx
+++ src/components/Protected/Sites/SiteChecks/AirConditioningRecurrenceCheck.jsx
@@ -518,7 +518,7 @@ const AirConditioningRecurrenceCheck = ({
 
             if (parentFoldersResponse?.parentFolders?.length > 0) {
                 const logBooksFolder = parentFoldersResponse.parentFolders.find(
-                    folder => folder.name.trim() === 'Log Books'
+                    folder => folder.name.trim() === '6 - Log Books'
                 );
 
                 if (logBooksFolder) {
@@ -568,7 +568,7 @@ const AirConditioningRecurrenceCheck = ({
 
             if (parentFoldersResponse?.parentFolders?.length > 0) {
                 const logBooksFolder = parentFoldersResponse.parentFolders.find(
-                    folder => folder.name.trim() === 'Log Books'
+                    folder => folder.name.trim() === '6 - Log Books'
                 );
 
                 if (logBooksFolder) {
