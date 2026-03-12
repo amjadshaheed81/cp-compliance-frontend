a775a29691b91a666afbf709fffcac78837e8f7e
pratulcodes <pratul.makar@tdkglobal.com>
2026-03-12 12:59:19 +0530

    test cases

--- src/utils/browserCheck.js
+++ src/utils/browserCheck.js
@@ -3,9 +3,9 @@ import bowser from 'bowser';
 export const isBrowserSupported = () => {
   const browser = bowser.getParser(window.navigator.userAgent);
   return browser.satisfies({
-    chrome: '>90',
-    firefox: '>60',
-    edge: '>15',
-    safari: '>10',
+    chrome: '>=100',
+    firefox: '>=100',
+    edge: '>=100',
+    safari: '>=15',
   });
 };
